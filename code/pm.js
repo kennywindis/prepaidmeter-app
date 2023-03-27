da7fb920f5726dd364397f2d62f8403ba9c2264c
kennywindis <kennyphilip100@gmail.com>
2023-03-27 15:47:59 +0100

    pm

--- code/pm.js
+++ code/pm.js
@@ -1,17 +1,17 @@
-    var units;
+var units;
 var amount=prompt("enter amount of unit(s) to be purchased");
 
-if (amount >= 2000){
+if (amount >= 0){
 units = amount/10.5;
 alert("your unit(s) purchased is" + " " + units + "watts");
 }
 
-else if (amount >= 4000){
+else if (amount >= 1000){
 units = amount/12.5;
 alert("your unit(s) purchased is" + " " + units + "watts");
 }
 
-else if (amount >= 6000){
+else if (amount >= 3000){
 units = amount/14.5;
 alert("your unit(s) purchased is" + " " + units + "watts");
 }
